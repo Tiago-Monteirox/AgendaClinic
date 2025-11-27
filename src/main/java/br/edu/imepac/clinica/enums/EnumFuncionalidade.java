f6e74554699776933c80881c29ccf3343ff4bb8a
Tiago-Monteirox <tiagomonteirodev@outlook.com>
2025-11-26 21:02:03 -0300

    Adicionado EnumFuncionalidade.java

---
+++ src/main/java/br/edu/imepac/clinica/enums/EnumFuncionalidade.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.imepac.clinica.enums;
+
+/**
+ *
+ * @author tiago-monteiro
+ */
+public enum EnumFuncionalidade {
+    GERENCIAR_MEDICOS,
+    GERENCIAR_ESPECIALIDADES,
+    GERENCIAR_PACIENTES,
+    AGENDAR_CONSULTAS,
+    VISUALIZAR_PRONTUARIOS,
+    GERENCIAR_USUARIOS;
+}
